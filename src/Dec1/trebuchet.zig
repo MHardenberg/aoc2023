090a629a4a3fd43fabe49e72f44dfd908cd711c7
Michel Hardenberg <MHardenberg@proton.me>
2023-12-18 09:26:33 +0100

    right setup - wrong output

--- src/Dec1/trebuchet.zig
+++ src/Dec1/trebuchet.zig
@@ -1,9 +1,34 @@
 const std = @import("std");
 const utils = @import("../utils.zig");
+const digits = [_]u8{ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+const line_break = '\n';
 
 pub fn calibrate() !void {
     std.debug.print("Calibrating...\n", .{});
+
     const input_string = try utils.readFile("./src/Dec1/input");
 
-    std.debug.print("Input {s}\n", .{input_string});
+    var in_digits: [2]u8 = undefined;
+    var current_idx: u8 = 0;
+    var line_sum: u16 = 0;
+
+    for (input_string) |in_char| {
+        if (in_char == line_break) {
+            var out = try std.fmt.parseInt(u8, &in_digits, 10);
+            line_sum += out;
+            current_idx = 0;
+        }
+
+        const is_digit = utils.linSearch(u8, &digits, in_char);
+        if (is_digit) {
+            in_digits[current_idx] = in_char;
+            if (current_idx == 0) {
+                current_idx += 1;
+            }
+        }
+    }
+
+    std.debug.print("Sum of lines: {}\n", .{line_sum});
+    const allocator = std.heap.page_allocator;
+    defer allocator.free(input_string);
 }
--- src/utils.zig
+++ src/utils.zig
@@ -1,17 +1,26 @@
 const std = @import("std");
 
+pub fn linSearch(comptime T: type, haystack: []const T, needle: T) bool {
+    for (haystack) |thing| {
+        if (thing == needle) {
+            return true;
+        }
+    }
+    return false;
+}
+
 pub fn getCWD() ![]const u8 {
     var buf: [std.fs.MAX_PATH_BYTES]u8 = undefined;
     const cwd = try std.os.getcwd(&buf);
     return cwd;
 }
 
+// hands ownership to caller
 pub fn readFile(file_path: []const u8) ![]const u8 {
     const allocator = std.heap.page_allocator;
     const max_bytes = std.math.maxInt(usize);
 
     var data = try std.fs.cwd().readFileAlloc(allocator, file_path, max_bytes);
-    defer allocator.free(data);
 
     return data;
 }
