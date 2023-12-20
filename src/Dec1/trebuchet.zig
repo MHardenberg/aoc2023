60772c58be79a80371b7e5766c818ea6e5bdb43d
Michel Hardenberg <MHardenberg@proton.me>
2023-12-20 14:03:31 +0100

    reworked dec1

--- src/Dec1/trebuchet.zig
+++ src/Dec1/trebuchet.zig
@@ -1,46 +1,40 @@
 const std = @import("std");
 const utils = @import("../utils.zig");
-const digits = [_]u8{ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
-const line_break = '\n';
+const allocator = std.heap.page_allocator;
 
 pub fn calibrate() !void {
     std.debug.print("Calibrating...\n", .{});
-    const allocator = std.heap.page_allocator;
-    const input_string = try utils.readFile("./src/Dec1/input");
-    defer allocator.free(input_string);
+    const input = try utils.readFile("./src/Dec1/input");
+    defer allocator.free(input);
 
-    var line_buffer: [100]u8 = undefined;
-    var digit_buffer: [2]u8 = undefined;
-    var line_idx: u8 = 0;
-    var digit_idx: u8 = 0;
-    var line_sum: u16 = 0;
+    var result: u16 = 0;
+    var iterator = std.mem.splitScalar(u8, input, '\n');
 
-    for (input_string) |in_char| {
-        if (in_char == line_break and digit_idx > 0) {
-            var out = try std.fmt.parseInt(u8, &digit_buffer, 10);
-            line_sum += out;
-
-            std.debug.print("{s}:    ", .{line_buffer});
-            std.debug.print("{}\n", .{out});
-
-            for (0..line_idx) |index| {
-                line_buffer[index] = undefined;
-            }
-            line_idx = 0;
-            digit_idx = 0;
+    while (iterator.next()) |line| {
+        if (line.len == 0) {
+            continue;
         }
 
-        line_buffer[line_idx] = in_char;
-        line_idx += 1;
+        var first_digit: ?u8 = null;
+        var second_digit: ?u8 = null;
 
-        const is_digit = utils.linSearch(u8, &digits, in_char);
-        if (is_digit) {
-            digit_buffer[digit_idx] = in_char;
-            if (digit_idx == 0) {
-                digit_buffer[1] = in_char;
-                digit_idx += 1;
+        for (line) |char| {
+            if (!std.ascii.isDigit(char)) {
+                continue;
             }
+
+            if (first_digit == null) {
+                first_digit = char;
+            } else {
+                second_digit = char;
+            }
+        }
+
+        if (first_digit) |first| {
+            const number_str = [2]u8{ first, second_digit orelse first };
+            const parseResult = try std.fmt.parseUnsigned(u8, &number_str, 10);
+            result += parseResult;
         }
     }
-    std.debug.print("Sum of lines: {}\n", .{line_sum});
+    std.debug.print("results: {}\n", .{result});
 }
