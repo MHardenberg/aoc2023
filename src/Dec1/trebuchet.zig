6a9ae38b13db8405e159ba9cab997a4f9fe765c3
Michel Hardenberg <MHardenberg@proton.me>
2023-12-20 11:25:19 +0100

    part 1 works

--- src/Dec1/trebuchet.zig
+++ src/Dec1/trebuchet.zig
@@ -5,30 +5,42 @@ const line_break = '\n';
 
 pub fn calibrate() !void {
     std.debug.print("Calibrating...\n", .{});
-
+    const allocator = std.heap.page_allocator;
     const input_string = try utils.readFile("./src/Dec1/input");
+    defer allocator.free(input_string);
 
-    var in_digits: [2]u8 = undefined;
-    var current_idx: u8 = 0;
+    var line_buffer: [100]u8 = undefined;
+    var digit_buffer: [2]u8 = undefined;
+    var line_idx: u8 = 0;
+    var digit_idx: u8 = 0;
     var line_sum: u16 = 0;
 
     for (input_string) |in_char| {
-        if (in_char == line_break) {
-            var out = try std.fmt.parseInt(u8, &in_digits, 10);
+        if (in_char == line_break and digit_idx > 0) {
+            var out = try std.fmt.parseInt(u8, &digit_buffer, 10);
             line_sum += out;
-            current_idx = 0;
+
+            std.debug.print("{s}:    ", .{line_buffer});
+            std.debug.print("{}\n", .{out});
+
+            for (0..line_idx) |index| {
+                line_buffer[index] = undefined;
+            }
+            line_idx = 0;
+            digit_idx = 0;
         }
+
+        line_buffer[line_idx] = in_char;
+        line_idx += 1;
 
         const is_digit = utils.linSearch(u8, &digits, in_char);
         if (is_digit) {
-            in_digits[current_idx] = in_char;
-            if (current_idx == 0) {
-                current_idx += 1;
+            digit_buffer[digit_idx] = in_char;
+            if (digit_idx == 0) {
+                digit_buffer[1] = in_char;
+                digit_idx += 1;
             }
         }
     }
-
     std.debug.print("Sum of lines: {}\n", .{line_sum});
-    const allocator = std.heap.page_allocator;
-    defer allocator.free(input_string);
 }
