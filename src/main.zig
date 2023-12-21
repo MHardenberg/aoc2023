3d344dcd77644711ef5043142525d4e788647dd4
Michel Hardenberg <MHardenberg@proton.me>
2023-12-21 22:54:09 +0100

    started on day2

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
 const Dec1 = @import("Dec1/trebuchet.zig");
+const Dec2 = @import("Dec2/cubes.zig");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("Running AoC 2023 solutions ...\n", .{});
-    try Dec1.calibrate();
+    //try Dec1.calibrate();
+    try Dec2.possible_games();
 }
