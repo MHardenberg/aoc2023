6ad2624d2e31a6bb913d33e164f638186096321c
Michel Hardenberg <MHardenberg@proton.me>
2023-12-21 23:05:42 +0100

    some rework

--- src/Dec2/cubes.zig
+++ src/Dec2/cubes.zig
@@ -2,17 +2,12 @@ const std = @import("std");
 const utils = @import("../utils.zig");
 const allocator = std.heap.page_allocator;
 
-pub fn parse_game_num(line: []const u8) !?u8 {
-    var line_iter = std.mem.splitScalar(u8, line, ':');
-    const game = line_iter.next();
-    if (game) |g| {
-        if (g.len == 0) {
-            return null;
-        }
-        const num_str = g[5..];
-        return try std.fmt.parseUnsigned(u8, num_str, 10);
+pub fn parse_game_num(game_str: []const u8) !?u8 {
+    if (game_str.len == 0) {
+        return null;
     }
-    return null;
+    const num_str = game_str[5..];
+    return try std.fmt.parseUnsigned(u8, num_str, 10);
 }
 
 pub fn possible_games() !void {
@@ -23,12 +18,13 @@ pub fn possible_games() !void {
     var game_num: ?u8 = undefined;
     var iterator = std.mem.splitScalar(u8, input, '\n');
     while (iterator.next()) |line| {
-        game_num = try parse_game_num(line);
-        if (game_num) |num| {
-            std.debug.print("Game number: {}\n", .{num});
-            var line_iter = std.fmt.parseInt(u8, line);
-            _ = line_iter;
-            // discard game num
+        var line_iter = std.mem.splitScalar(u8, line, ':');
+        var game_str = line_iter.next();
+        if (game_str) |gs| {
+            game_num = try parse_game_num(gs);
+            if (game_num) |gn| {
+                std.debug.print("Game number: {}\n", .{gn});
+            }
         }
     }
 }
